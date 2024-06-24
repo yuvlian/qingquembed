f28ff3787acc3f2dc3fe2f8e56aeb6b0129c5f25
Yulian <138542238+yuvlian@users.noreply.github.com>
2024-06-24 09:47:48 +0700

    1.3.0 - A slightly better code + Rename Project
    - Removed unnecessary borrows
    - Removed unnecessary statics
    - Make code a bit more readable
    - Renamed project to Qingquembed

--- README.md
+++ README.md
@@ -1,43 +1,43 @@
-# Rusty Embed Fixer Bot
-Simple discord embed fixer bot that can also provide download links. Written in rust using serenity.
+# Qingquembed [1.3.0]
+Qingquembed is a Discord bot that was written in Rust using serenity.
 
-I don't host it except for my own discord server, but I do provide prebuilt binaries so you can host it yourself easily.
+Its purpose is to fix embed links of TikTok, Instagram, Twitter/X. It can also provide download links. In the next version, I will add support for YouTube downloads.
 
-## How to Use The Bot
-### Theres two methods:
+Why Rust, you may ask? Cause it's so damn fast, safe, and the binary is small and independent.
 
-1. Say anything
+The bot is named after the best girl from Honkai: Star Rail, Qingque.
 
-- As long as you have tiktok/instagram/twitter/x link(s) in your message, the bot will try to resend it. It won't re-reply or re-upload your attachment though. For tiktok slideshows, refer to the third command from below
+I only host this bot for myself, but I provide both Windows and Linux prebuilts so you can host it yourself easily.
 
-2. Actual commands
+## Bot Usage
+### There are two methods:
 
-- qdev
-  - returns version & dev name
-- qsay [something]
-  - make the bot say whatever you want (prebuilt does not have this command)
-- q[link] 
-  - If used with tiktok slideshow link, will send all image links in chunks of 5 (this embeds)
-  - If used with tiktok video link, will give download link (this doesn't embed). Just paste normally without prefix if you want the embed.
-  - If used with instagram reel link, will give download link (this embeds)
-  - Doesn't work well with instagram slideshows, as instafix only returns the first 4 images and is manipulated as a grid
-  - Doesnt support anything twitter related yet. Not planning to.
+**1. Say anything**
+  - The bot will use regex to find and replace the part of your message that contains TikTok/Instagram/Twitter/X URL and then resends it. Keep in mind it won't re-reply nor will it reupload the attachment of the message.
+  - Tiktok URLs will be replaced with VxTiktok, Instagram URLs with InstaFix, Twitter/X URLs with VxTwitter.
+  - For slideshows (e.g. TikTok slideshows), please refer to the actual commands number 3.
 
-## How to get the bot
-### Prebuilt
-For those who don't even know what Rust is, go with this instead!
-1. Download this https://github.com/yuvlian/rusty_embed_fixer_bot/releases/download/1.2.0/rusty_embed_fixer_bot.7z
-2. Extract the zip
-3. Edit config.yml to your actual discord bot token
-4. Launch the binary (aka .exe)
+**2. Actual commands**
+  - **qdev**
+    - Will send information about the bot version and developer.
+  - **qsay [string]**
+    - Will make the bot say something. Does not reply nor reupload attachments. This is completely removed in the prebuilt.
+  - **q[link]**
+    - If used with a TikTok slideshow link, it will give you embed URL of the images in chunks of 5.
+    - If used with a TikTok video link, it will give you the download link (no watermark). This link does not embed.
+    - If used with an Instagram video link (or reel), it will give you a download link that embeds. However, due to Discord's proxies, you can only play the video by opening it in browser or downloading it. Simply paste the Instagram link normally, if you only plan to play the video on Discord.
+    - This command does not work well with Instagram slideshows, due to InstaFix returning only the first four images as a grid, and will break if it's a video and image slideshow.
+    - Does not support Twitter/X yet. I am not planning to add support for it as I don't use it. 
 
-   (Yes I know it's named "rusty_tiktok_bot" despite supporting 2 other websites)
-5. As long as the binary is running, bot is online
+## How To Get The Bot
+### Prebuilt:
+unimplemented!()
 
-### Build from source
-I'm sure you already know how if you're planning to build from source
+### Build from source:
+I'm sure you already know how, if you are planning to build from source. And if you don't even know what Rust is, go with prebuilt.
 
-### Special thanks
-- folks behind serenity, tnktok.com, ddinstagram.com, vxtwitter.com, fixvx.com 
-- https://api.tiklydown.eu.org
-- xeondev
+### Special thanks to:
+- Folks behind Rust and the dependencies of this bot, especially Serenity.
+- Folks behind VxTiktok, VxTwitter, and InstaFix.
+- Folks behind https://tiklydown.eu.org.
+- And lastly, [thexeondev](https://github.com/thexeondev) for giving feedbacks on my code.
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,87 @@
+use std::{
+    fs::File,
+    io::BufReader,
+    sync::LazyLock,
+};
+use serde::Deserialize;
+use regex::Regex;
+use reqwest::Client as ReqClient;
 use serenity::{
     async_trait,
     model::channel::Message,
     prelude::*,
 };
-use regex::Regex;
-use reqwest::Client as ReqClient;
-use std::sync::LazyLock;
-use serde::Deserialize;
-use std::{
-    fs::File,
-    io::BufReader,
-};
 mod libtok; // import libtok.rs
 use libtok::TikTokApiResponse;
 
-struct Handler;
-
 // Regex to match links
-static TIKTOK_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"https?://(www\.)?(tiktok\.com|vt\.tiktok\.com|vm\.tiktok\.com)/?").unwrap());
-static INSTAGRAM_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"https?://(www\.)?(instagram\.com|instagr\.am)/?").unwrap());
-static TWITTER_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"https?://(www\.)?(twitter\.com)/?").unwrap());
-static X_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"https?://(www\.)?(x\.com)/?").unwrap());
+static TIKTOK_REGEX: LazyLock<Regex> = LazyLock::new(|| 
+    Regex::new(r"https?://(www\.)?(tiktok\.com|vt\.tiktok\.com|vm\.tiktok\.com)/?").unwrap()
+);
+static INSTAGRAM_REGEX: LazyLock<Regex> = LazyLock::new(|| 
+    Regex::new(r"https?://(www\.)?(instagram\.com|instagr\.am)/?").unwrap()
+);
+static TWITTER_REGEX: LazyLock<Regex> = LazyLock::new(|| 
+    Regex::new(r"https?://(www\.)?(twitter\.com)/?").unwrap()
+);
+static X_REGEX: LazyLock<Regex> = LazyLock::new(|| 
+    Regex::new(r"https?://(www\.)?(x\.com)/?").unwrap()
+);
 
 // Static strings to replace the matched stuff
-static TNKTOK: LazyLock<String> = LazyLock::new(|| String::from("https://tnktok.com/"));
-static DDINSTAGRAM: LazyLock<String> = LazyLock::new(|| String::from("https://ddinstagram.com/"));
-static VXTWITTER: LazyLock<String> = LazyLock::new(|| String::from("https://vxtwitter.com/"));
-static FIXVX: LazyLock<String> = LazyLock::new(|| String::from("https://fixvx.com/"));
-static D_DDINSTAGRAM: LazyLock<String> = LazyLock::new(|| String::from("https://d.ddinstagram.com/"));
+static VXTIKTOK: LazyLock<String> = LazyLock::new(|| 
+    String::from("https://vt.vxtiktok.com/")
+);
+static DDINSTAGRAM: LazyLock<String> = LazyLock::new(|| 
+    String::from("https://ddinstagram.com/")
+);
+static VXTWITTER: LazyLock<String> = LazyLock::new(|| 
+    String::from("https://vxtwitter.com/")
+);
+static FIXVX: LazyLock<String> = LazyLock::new(|| 
+    String::from("https://fixvx.com/")
+);
+static D_DDINSTAGRAM: LazyLock<String> = LazyLock::new(|| 
+    String::from("https://d.ddinstagram.com/")
+);
 
 // Reqwest Client for d.ddinstagram thing
-static T_BOT_CLIENT: LazyLock<ReqClient> = LazyLock::new(|| ReqClient::builder().user_agent("TelegramBot").build().unwrap());
+static T_BOT_CLIENT: LazyLock<ReqClient> = LazyLock::new(|| 
+    ReqClient::builder().user_agent("TelegramBot").build().unwrap()
+);
 
-// Allowed users for "qsay" command
-static USER1: LazyLock<String> = LazyLock::new(|| String::from("876725552474644490"));
+// Allowed user for "qsay" command
+static USER_1: LazyLock<String> = LazyLock::new(|| 
+    String::from("932589665230401566")
+);
 
-// Prefixes
-static PREFIX_QSAY: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^\bqsay\b").unwrap());
-static PREFIX_QDEV: LazyLock<String> = LazyLock::new(|| String::from("qdev"));
-static PREFIX_Q: LazyLock<String> = LazyLock::new(|| String::from("qhttp")); // Added "http" incase someone says stupid shit like quantum [link]
-
-// tiklydown.eu.org API Base URL
-static API_TIKLYDOWN_EU_ORG: LazyLock<String> = LazyLock::new(|| String::from("https://api.tiklydown.eu.org/api/download/v2?url="));
+// Actual commands
+static PREFIX_QSAY: LazyLock<String> = LazyLock::new(|| 
+    String::from("qsay")
+);
+static PREFIX_QDEV: LazyLock<String> = LazyLock::new(|| 
+    String::from("qdev")
+);
+static PREFIX_Q: LazyLock<String> = LazyLock::new(|| 
+    String::from("qhttp") // Added "http" incase someone says stupid shit like quantum [link]
+); 
 
 // Version & Dev of Bot
-static DEV: LazyLock<String> = LazyLock::new(|| String::from("[version]\nrust = \"1.80.0-nightly\"\nserenity = \"0.12.2\"\nthis_bot = \"1.2.0\"\n\n[source]\ndev = \"yulian\"\nrepo = \"<https://github.com/yuvlian/rusty_embed_fixer_bot>\"\nlicense = \"bsd-3-clause\""));
+fn dev_info() -> String {
+String::from(
+r#"[version]
+rust = "1.81.0-nightly"
+serenity = "0.12.2"
+qingquembed = "1.3.0"
+
+[source]
+dev = "yulian"
+repo = "<https://github.com/yuvlian/rusty_embed_fixer_bot>"
+license = "bsd-3-clause"
+"#)
+}
+
+struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -55,89 +92,97 @@ impl EventHandler for Handler {
             return;
         }
 
+        // Get user_id
+        let user_id = msg.author.id;
 
         // Version check & dev.
         // Example usage: qdev
         if msg.content == *PREFIX_QDEV {
-            if let Err(why) = msg.channel_id.say(&ctx.http, &*DEV).await {
-                println!("Error sending message: {why:?}");
+            if let Err(why) = msg.channel_id.say(&ctx.http, 
+                dev_info()
+            ).await {
+                println!("Error sending message: {:?}", why);
             }
         }
         
 
-        // Gets author id
-        let allowed_user = &msg.author.id.to_string();
-
-        // Makes the bot say something. Only allowed for certain users ;)
+        // Makes the bot say something. Whitelist is hardcoded.
         // Example usage: qsay Hello World!
-        // This is disabled in the prebuilt.
-        // Be sure to change or remove this to your liking if you're building from source!
-        // Otherwise... I will prank you! ^w^
-        if PREFIX_QSAY.is_match(&msg.content) && &*USER1 == allowed_user {
-            
-            // Bot sends the message
-              if let Err(why) = msg.channel_id.say(&ctx.http, PREFIX_QSAY.replace_all(&msg.content, "")).await {
-                  println!("Error sending message: {:?}", why);
-             }
+        // This (and anything related to it) is removed in the prebuilt.
+        if msg.content.starts_with(&*PREFIX_QSAY) {
+            let allowed_user = user_id.to_string();
+            if allowed_user == *USER_1 {
+                // Bot deletes the original message
+                if let Err(why) = msg.delete(&ctx.http).await {
+                    println!("Error deleting message: {:?}", why);
+                }
 
-            // Bot deletes the original message
-              if let Err(why) = msg.delete(&ctx.http).await {
-                 println!("Error deleting message: {:?}", why);
-             }
-          }
-
+                // Bot sends the message
+                if let Err(why) = msg.channel_id.say(&ctx.http,
+                    PREFIX_QSAY.replace(&msg.content, "")
+                ).await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+        }
 
         // TikTok Scraper. Sends image embed links if slideshow, sends download link to video if video. 
         // You can just paste without adding "q" before the link if you want to play a video.
         // Example usage: qhttps://www.tiktok.com/@xalbierblx/video/7356560922002951456
         if msg.content.starts_with(&*PREFIX_Q) && TIKTOK_REGEX.is_match(&msg.content) {
-
-            // Notify that the bot detects the message.
-            if let Err(why) = msg.channel_id.say(&ctx.http, format!("[<@{}>] Processing TikTok link: <{}>", msg.author.id, &msg.content[1..])).await {
-                println!("Error sending message: {why:?}");
-            }
-
             // Deletes original message
             if let Err(why) = msg.delete(&ctx.http).await {
                 println!("Error deleting message: {:?}", why);
             }
 
+            // Notify that the bot detects the message.
+            if let Err(why) = msg.channel_id.say(&ctx.http,
+                format!("[<@{}>] Processing TikTok link: <{}>", user_id, &msg.content[1..])
+            ).await {
+                println!("Error sending message: {:?}", why);
+            }
+
             // Formats base url + url from message
-            let url = format!("{}{}", *API_TIKLYDOWN_EU_ORG, &msg.content[1..]); 
+            let url = format!("https://api.tiklydown.eu.org/api/download/v2?url={}", &msg.content[1..]); 
 
             // Handle URL result from API
             match TikTokApiResponse::from_url(&url).await {
                 Ok(api_response) => {
-
                     // If success
                     let media_urls = api_response.get_media_urls();
                     if !media_urls.is_empty() {
+                        // Log the URL
                         println!("Media URLs found: {:?}", media_urls);
 
                         // Chunks so that it sends up to 5 media links per message
                         for chunk in media_urls.chunks(5) {
-
                             // Create new message
                             let mut message = String::new();
-
                             // Format and append each media link to the message
                             for (index, media_url) in chunk.iter().enumerate() {
-                                let content_index = index + 1; 
-                                let formatted_content = format!("[Content {}]({}) ", content_index, media_url);
+                                let content_index = index + 1;
+                                let formatted_content = format!(
+                                    "[Content {}]({}) ", content_index, media_url
+                                );
                                 message.push_str(&formatted_content);
                             }
 
                             // Send formatted message
-                            if let Err(why) = msg.channel_id.say(&ctx.http, message).await {
+                            if let Err(why) = msg.channel_id.say(&ctx.http, 
+                                message
+                            ).await {
                                 println!("Error sending message: {:?}", why);
                             }
                         }
                     } else {
-
                         // If fail to find media
                         println!("No media URLs found.");
-                        if let Err(why) = msg.channel_id.say(&ctx.http, "No media URL found. Are you sure the TikTok link is valid?").await {
-                            println!("Error sending no media URL found message: {:?}", why);
+
+                        // Inform the user
+                        if let Err(why) = msg.channel_id.say(&ctx.http,
+                            "No media URL found. Are you sure the TikTok link is valid?"
+                        ).await {
+                            println!("Error sending message: {:?}", why);
                         }
                     }
                 }
@@ -145,8 +190,12 @@ impl EventHandler for Handler {
                 // If fetching fails
                 Err(why) => {
                     println!("Error fetching TikTok data: {:?}", why);
-                    if let Err(err_msg) = msg.channel_id.say(&ctx.http, format!("Failed to fetch TikTok data: {:?}", why)).await {
-                        println!("Error sending failure message: {:?}", err_msg);
+
+                    // Inform the user
+                    if let Err(err_msg) = msg.channel_id.say(&ctx.http,
+                        format!("Failed to fetch TikTok data: {:?}", why)
+                    ).await {
+                        println!("Error sending message: {:?}", err_msg);
                     }
                 }
             }
@@ -159,15 +208,16 @@ impl EventHandler for Handler {
         // Good for downloading singular image or reel/video though.
         // Usage example: qhttps://instagram.com/reel/C53oGPULGk0/
 	    if msg.content.starts_with(&*PREFIX_Q) && INSTAGRAM_REGEX.is_match(&msg.content) {
-
-            // Notify that the bot detects the message
-            if let Err(why) = msg.channel_id.say(&ctx.http, format!("[<@{}>] Processing Instagram link: <{}>", msg.author.id, &msg.content[1..])).await {
-                println!("Error sending message: {why:?}");
-            }
-
             // Deletes original message
             if let Err(why) = msg.delete(&ctx.http).await {
                 println!("Error deleting message: {:?}", why);
+            }
+
+            // Notify that the bot detects the message
+            if let Err(why) = msg.channel_id.say(&ctx.http,
+                format!("[<@{}>] Processing Instagram link: <{}>", user_id, &msg.content[1..])
+            ).await {
+                println!("Error sending message: {why:?}");
             }
 
             // convert original url to d.ddinstagram
@@ -175,18 +225,24 @@ impl EventHandler for Handler {
 
             // Start fetching response
             match T_BOT_CLIENT.get(&*d_dd_url).send().await {
+                // If success
                 Ok(response) => {
                     let response_url = response.url().to_string();
 
+                    println!("Media URL: {}", response_url);
                     // Send response to discord
-                    if let Err(why) = msg.channel_id.say(&ctx.http, format!("[Content 1]({})", &response_url)).await {
+                    if let Err(why) = msg.channel_id.say(&ctx.http,
+                        format!("[Download]({})", response_url)
+                    ).await {
                         println!("Error sending message: {:?}", why);
                     }
                 }
                 // If fail
                 Err(why) => {
                     println!("Error fetching URL: {:?}", why);
-                    if let Err(err_msg) = msg.channel_id.say(&ctx.http, format!("Failed to fetch Instagram data: {:?}", why)).await {
+                    if let Err(err_msg) = msg.channel_id.say(&ctx.http,
+                        format!("Failed to fetch Instagram data: {:?}", why)
+                    ).await {
                         println!("Error sending failure message: {:?}", err_msg);
                     }
                 }
@@ -198,54 +254,54 @@ impl EventHandler for Handler {
         // This will automatically resend your messages that contains a tiktok/instagram/twitter/x link 
         // The resent message will have said links replaced with the embed fixed links
         // Note: It does not re-reply or reupload attachments
-        if !msg.content.starts_with(&*PREFIX_Q) && TIKTOK_REGEX.is_match(&msg.content) || INSTAGRAM_REGEX.is_match(&msg.content) || TWITTER_REGEX.is_match(&msg.content) || X_REGEX.is_match(&msg.content) {
-            
-            // Replace the links
-            let tiktok_replace = TIKTOK_REGEX.replace_all(&msg.content, &*TNKTOK);
-            let instagram_replace = INSTAGRAM_REGEX.replace_all(&tiktok_replace, &*DDINSTAGRAM);
-            let twitter_replace = TWITTER_REGEX.replace_all(&instagram_replace, &*VXTWITTER);
-            let x_replace = X_REGEX.replace_all(&twitter_replace, &*FIXVX);
-            
-            // Add author to tell who sent the original message
-            let replaced_content = format!("[<@{}>] {}", msg.author.id, x_replace);
-            
-            // Send modified message
-            if let Err(why) = msg.channel_id.say(&ctx.http, replaced_content).await {
-                println!("Error sending message: {:?}", why);
-            }
+        if !msg.content.starts_with(&*PREFIX_Q) && TIKTOK_REGEX.is_match(&msg.content) 
+            || INSTAGRAM_REGEX.is_match(&msg.content) 
+            || TWITTER_REGEX.is_match(&msg.content) 
+            || X_REGEX.is_match(&msg.content) {
             
             // Delete the original message
             if let Err(why) = msg.delete(&ctx.http).await {
                 println!("Error deleting message: {:?}", why);
             }
+
+            // Replace the links
+            let tiktok_replace = TIKTOK_REGEX.replace_all(&msg.content, &*VXTIKTOK);
+            let instagram_replace = INSTAGRAM_REGEX.replace_all(&tiktok_replace, &*DDINSTAGRAM);
+            let twitter_replace = TWITTER_REGEX.replace_all(&instagram_replace, &*VXTWITTER);
+            let x_replace = X_REGEX.replace_all(&twitter_replace, &*FIXVX);
+            
+            // Add author to tell who sent the original message
+            let replaced_content = format!("[<@{}>] {}", user_id, x_replace);
+
+            // Send modified message
+            if let Err(why) = msg.channel_id.say(&ctx.http, 
+                replaced_content
+            ).await {
+                println!("Error sending message: {:?}", why);
+            }
+            
         }
     }
 }
 
-// config.yml Deserialization
+
 #[derive(Deserialize)]
 struct Config {
     token: String,
 }
 
-// Read config.yml for the token
-static CONFIG: LazyLock<Config> = LazyLock::new(|| {
-    let file = File::open("config.yml").expect("Config file not found");
-    let reader = BufReader::new(file);
-    serde_yml::from_reader(reader).expect("Error reading config")
-});
-
-// Gateway intents. This will make the bot read messages from servers and direct messages.
-static INTENTS: LazyLock<GatewayIntents> = LazyLock::new(|| {
-    GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES | GatewayIntents::MESSAGE_CONTENT
-});
-
 #[tokio::main]
 async fn main() {
-    println!("{}\n", *DEV);
+    let file = File::open("config.yml").expect("Config file not found");
+    let reader = BufReader::new(file);
+    let config: Config = serde_yml::from_reader(reader).expect("Error reading config");
+
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    println!("{}", dev_info());
     
     let mut client =
-        Client::builder(&*CONFIG.token, *INTENTS).event_handler(Handler).await.expect("Error creating client");
+        Client::builder(config.token, intents).event_handler(Handler).await.expect("Error creating client");
 
     if let Err(why) = client.start().await {
         println!("Client error: {why:?}");
